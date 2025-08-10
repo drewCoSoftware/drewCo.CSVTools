a1af6f6b89e464b401969b6b06dc8fc70ef7e442
Andrew Ritz <aritz@rugsdirect.com>
2025-08-10 13:42:40 -0400

    - Initial commit for nuget package project.

---
+++ BuildNugetPackage.ps1
@@ -0,0 +1,35 @@
+function Item-Exists($path) {
+
+    $x = Get-Item -Path $path -ErrorAction SilentlyContinue
+    return $x.Length -gt 0
+}
+
+
+# Get a clean copy of the build dir.
+if (Item-Exists("nuget-build")) {
+    Remove-Item "nuget-build" -Force -Recurse
+
+    New-Item "nuget-build" -ItemType "Directory"
+}
+New-Item "nuget-build\lib\net8.0" -ItemType "Directory"
+
+
+# Build our assemblies:
+dotnet build drewCo.CSVTools.sln -p:Configuration=Release
+
+
+#Copy the stuff we care about to the lib folder....
+
+# NETCORE 8.0
+Copy-Item -Path ".\drewCo.CSVTools.Core\bin\Release\net8.0\drewCo.CSVTools.dll" "nuget-build\lib\net8.0\drewCo.CSVTools.dll"
+Copy-Item -Path ".\drewCo.CSVTools.Core\bin\Release\net8.0\drewCo.CSVTools.pdb" "nuget-build\lib\net8.0\drewCo.CSVTools.pdb"
+Copy-Item -Path ".\drewCo.CSVTools.Core\bin\Release\net8.0\drewCo.CSVTools.xml" "nuget-build\lib\net8.0\drewCo.CSVTools.xml"
+Copy-Item -Path ".\drewCo.CSVTools.Core\bin\Release\net8.0\drewCo.CSVTools.deps.json" "nuget-build\lib\net8.0\drewCo.CSVTools.deps.json"
+
+
+# Copy everything to the build dir....
+# Copy-Item -Path ".\lib" ".\nuget-build\lib" -Recurse
+Copy-Item ".\drewCo.CSVTools.nuspec" ".\nuget-build\drewCo.CSVTools.nuspec"
+
+# Pack it all up...
+nuget pack ".\nuget-build\drewCo.CSVTools.nuspec"
---
+++ drewCo.CSVTools/CSVTools.cs
@@ -0,0 +1,69 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace drewCo.CsvTools;
+
+// ==============================================================================================================================
+/// <summary>
+/// Convenience functions for CsvHelper features.
+/// </summary>
+// TODO: We should make a silly little nuget package for these funtions, just so they don't have to be copied all over the place.
+public static class CsvTools
+{
+
+    // ------------------------------------------------------------------------------------------------------------
+    // TODO: SHARE:
+    public static List<TData> ReadCSV<TData>(string usePath)
+    {
+        using (var fs = new StreamReader(usePath))
+        using (var r = new CsvReader(fs, CultureInfo.InvariantCulture))
+        {
+            var allLines = r.GetRecords<TData>().ToList();
+            return allLines;
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------------------
+    // TODO: SHARE:
+    // This version should be used when you don't want to close the underlying stream.
+    public static List<TData> ReadCSV<TData>(Stream s)
+    {
+        using (var fs = new StreamReader(s, leaveOpen: true))
+        using (var r = new CsvReader(fs, CultureInfo.InvariantCulture))
+        {
+            var allLines = r.GetRecords<TData>().ToList();
+            return allLines;
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------------------
+    // TODO: SHARE:
+    public static void AppendCSVData<TData>(Stream s, TData toAppend)
+    {
+        AppendCSVData<TData>(s, new[] { toAppend });
+    }
+
+    // ------------------------------------------------------------------------------------------------------------
+    // TODO: SHARE:
+    public static void AppendCSVData<TData>(Stream s, IEnumerable<TData> toAppend)
+    {
+        bool isFirst = s.Length == 0;
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = isFirst,
+        };
+
+        using (var fs = new StreamWriter(s, leaveOpen: true))
+        using (var w = new CsvWriter(fs, config, true))
+        {
+            // Will this work?
+            if (!isFirst)
+            {
+                s.Seek(0, SeekOrigin.End);
+            }
+            w.WriteRecords(toAppend);
+        }
+    }
+}
